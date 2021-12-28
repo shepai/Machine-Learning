9abece2241ce34ed811f4f4af36c89d08d23d6f8
shepai <dextershepherd@hotmail.co.uk>
2021-12-28 10:27:43 +0000

    extract all the data

--- FootballPrediction/dataDownloader.py
+++ FootballPrediction/dataDownloader.py
@@ -10,26 +10,21 @@ from xml.etree import ElementTree as ET
 
 class downloader:
     def __init__(self,name="360_data.xml"):
-        xml = ET.parse(name)
-        root_element = xml.getroot()
-        for child in root_element:
-            print(self.xml_dict(child))
-    def xml_dict(self,node, path="", dic =None):
-        if dic == None:
-            dic = {}
-        name_prefix = path + ("." if path else "") + node.tag
-        numbers = set()
-        for similar_name in dic.keys():
-            if similar_name.startswith(name_prefix):
-                numbers.add(int (similar_name[len(name_prefix):].split(".")[0] ) )
-        if not numbers:
-            numbers.add(0)
-        index = max(numbers) + 1
-        name = name_prefix + str(index)
-        dic[name] = node.text + "<...>".join(childnode.tail
-                                             if childnode.tail is not None else
-                                             "" for childnode in node)
-        for childnode in node:
-            self.xml_dict(childnode, name, dic)
-        return dic
+        tree = ET.parse(name)
+        root = tree.getroot()
+        self.struct={}
+        print(root.findall('SoccerDocument'))
+        for graph in root.findall('SoccerDocument'):
+                   for g in graph.findall('PlayerChanges'): #check the words saved
+                       for team in g.findall('Team'):
+                           for player in team.findall('Player'):
+                               name=player.find('Name').text
+                               position=player.find('Position').text
+                               print(name,position)
+                               data=[]
+                               for i in player.findall("Stat"):
+                                   data.append(i.text)
+                               print(data)
+                                    
+                   
 downloader()
