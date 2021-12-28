d635b25214e2ef8ed15f3e479f9384c2ef9a68a5
shepai <dextershepherd@hotmail.co.uk>
2021-12-28 15:49:07 +0000

    new code for 360 dataset

--- FootballPrediction/dataDownloader.py
+++ FootballPrediction/dataDownloader.py
@@ -7,24 +7,44 @@ Code by Dexter R Shepherd, aged 20
 """
 
 from xml.etree import ElementTree as ET
+import json
 
 class downloader:
-    def __init__(self,name="360_data.xml"):
+    def __init__(self,name="playerData.xml",Doc_360="360Data.json"):
         tree = ET.parse(name)
         root = tree.getroot()
         self.struct={}
-        print(root.findall('SoccerDocument'))
+        #gather player information
         for graph in root.findall('SoccerDocument'):
                    for g in graph.findall('PlayerChanges'): #check the words saved
                        for team in g.findall('Team'):
                            for player in team.findall('Player'):
                                name=player.find('Name').text
                                position=player.find('Position').text
-                               print(name,position)
+                               #print(name,position)
                                struct={}
                                for i,dat in enumerate(player.findall("Stat")):
                                    struct[dat.attrib['Type']]=dat.text
-                               print(struct)
+                               if self.struct.get(player,False)==False:
+                                   #add if not in array
+                                   self.struct[player]=struct
+                               else: #traceback output
+                                   print("found duplicate data for", name, "keeping first")
+                                   print(self.struct[player])
+        #create table
+        self.TABLE=[]
+        #TABLE entry [player, zone, pressure, distance of shot, success, foot]      
+        #gather 360 data
+        file=open(Doc_360) #read file
+        r=file.read()
+        file.close()
+        moves = json.loads(r) #convert to dictionary
+        for event in moves:
+            for key in event:
+                print(key,event[key])
+            break
+        
+       
                                     
                    
 downloader()
